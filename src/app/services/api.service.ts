add4c773b430a0fcaa3bf5acf82c20a7087b5445
Jorge Arias <jorgearias.ja@gmail.com>
2026-05-10 00:13:27 -0500

    feat: configure production environment API URL

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,12 +1,13 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  private readonly baseUrl = 'http://localhost:5126/api/';
+  private readonly baseUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
